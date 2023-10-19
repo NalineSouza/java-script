560d1f4cb73c09a9197bf6164ab5a3af0cc101d5
Naline Souza <ketillennalinesouza@gmail.com>
2023-10-19 17:32:59 -0400

    condicoes aninhadas em javascript

---
+++ cursojavascript/aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 20
+if ( idade < 16) {
+    console.log ('Não Vota')
+} else {
+    if (idade >= 16 && idade < 18) {
+        console.log('Voto Opcional')
+    } else {
+        console.log('Voto Obrigatório')
+    }
+
+}

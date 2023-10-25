4d5c3b542b5c7ed501391db7d13fb90c53521aea
Naline Souza <ketillennalinesouza@gmail.com>
2023-10-25 07:49:34 -0400

    exercicios de javascript

--- cursojavascript/aula12/ex011.js
+++ cursojavascript/aula12/ex011.js
@@ -1,11 +1,10 @@
-var idade = 20
+var idade = 22
+console.log(`Você tem ${idade} anos.`)
 if ( idade < 16) {
     console.log ('Não Vota')
+} else if (idade < 18 ||  idade > 65) {
+     console.log('Voto Opcional') 
 } else {
-    if (idade >= 16 && idade < 18) {
-        console.log('Voto Opcional')
-    } else {
-        console.log('Voto Obrigatório')
-    }
-
+     console.log('Voto Obrigatório')
 }
+

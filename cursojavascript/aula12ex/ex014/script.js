ce5089c2543f497af3df30053db4402b504c7258
Naline Souza <ketillennalinesouza@gmail.com>
2023-10-26 13:57:49 -0400

    exercicio de javascript

--- cursojavascript/aula12ex/ex014/script.js
+++ cursojavascript/aula12ex/ex014/script.js
@@ -1,1 +1,10 @@
-alert('Olá!')
+function carregaar() {
+      var msg = window.document.getElememtById('msg')
+      var img = window.document.getElementById('imagem')
+      var data = new Date()
+      var hora = data.getHours()
+      msg.innerHTML = `Agora são ${hora} horas.`
+      if (hora >= 0 && hora < 12) {
+        //Bom Dia!
+      }
+}
